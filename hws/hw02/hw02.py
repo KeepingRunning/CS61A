914253dba97f3d8f1f6da7b7d3958eded25685d8
Keep_running <1599949878@qq.com>
2024-10-24 14:12:17 +0800

    finished hw02

--- hws/hw02/hw02.py
+++ hws/hw02/hw02.py
@@ -1,4 +1,4 @@
-from operator import add, mul, sub
+from operator import add, mul
 
 square = lambda x: x * x
 
@@ -12,10 +12,23 @@ increment = lambda x: x + 1
 HW_SOURCE_FILE=__file__
 
 
+def square(x):
+    return mul(x, x)
+
+def increment(x):
+    return (x + 1)
+
+def identity(x):
+    return x
+
+def triple(x):
+    return mul(x, 3)
+
 def product(n, term):
     """Return the product of the first n terms in a sequence.
-    n -- a positive integer
-    term -- a function that takes one argument to produce the term
+
+    n: a positive integer
+    term:  a function that takes one argument to produce the term
 
     >>> product(3, identity)  # 1 * 2 * 3
     6
@@ -31,183 +44,83 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
-    assert isinstance(n, int) and n > 0, 'n must be a positive integer'
-    prod, i = 1, 1
-    while i <= n:
-        prod, i = prod * term(i), i + 1
-    return prod
+    ans = 1
+    for i in range(1, n + 1):
+        ans = mul(ans, term(i))
+    return ans
 
-
-def accumulate(combiner, base, n, term):
-    """Return the result of combining the first n terms in a sequence and base.
-    The terms to be combined are term(1), term(2), ..., term(n).  combiner is a
-    two-argument commutative function.
+def accumulate(fuse, start, n, term):
+    """Return the result of fusing together the first n terms in a sequence 
+    and start.  The terms to be fused are term(1), term(2), ..., term(n). 
+    The function fuse is a two-argument commutative & associative function.
 
     >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
     15
     >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
     26
-    >>> accumulate(add, 11, 0, identity) # 11
+    >>> accumulate(add, 11, 0, identity) # 11 (fuse is never used)
     11
     >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
     25
     >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
     72
+    >>> # 2 + (1^2 + 1) + (2^2 + 1) + (3^2 + 1)
     >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
     19
-    >>> accumulate(lambda x, y: 2 * (x + y), 2, 3, square)
-    58
-    >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
-    16
     """
     "*** YOUR CODE HERE ***"
-    acc, i = base, 1
-    while i <= n:
-        acc, i = combiner(acc, term(i)), i+1
-    return acc
-
+    ans = start
+    for i in range(1, n + 1):
+        ans = fuse(ans, term(i))
+    return ans
 
 def summation_using_accumulate(n, term):
-    """Returns the sum of term(1) + ... + term(n). The implementation
-    uses accumulate.
+    """Returns the sum: term(1) + ... + term(n), using accumulate.
 
-    >>> summation_using_accumulate(5, square)
+    >>> summation_using_accumulate(5, square) # square(1) + square(2) + ... + square(4) + square(5)
     55
-    >>> summation_using_accumulate(5, triple)
+    >>> summation_using_accumulate(5, triple) # triple(1) + triple(2) + ... + triple(4) + triple(5)
     45
-    >>> from construct_check import check
-    >>> # ban iteration and recursion
-    >>> check(HW_SOURCE_FILE, 'summation_using_accumulate',
-    ...       ['Recursion', 'For', 'While'])
-    True
+    >>> # This test checks that the body of the function is just a return statement.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
     return accumulate(add, 0, n, term)
-    
+
 
 def product_using_accumulate(n, term):
-    """An implementation of product using accumulate.
+    """Returns the product: term(1) * ... * term(n), using accumulate.
 
-    >>> product_using_accumulate(4, square)
+    >>> product_using_accumulate(4, square) # square(1) * square(2) * square(3) * square()
     576
-    >>> product_using_accumulate(6, triple)
+    >>> product_using_accumulate(6, triple) # triple(1) * triple(2) * ... * triple(5) * triple(6)
     524880
-    >>> from construct_check import check
-    >>> # ban iteration and recursion
-    >>> check(HW_SOURCE_FILE, 'product_using_accumulate',
-    ...       ['Recursion', 'For', 'While'])
-    True
+    >>> # This test checks that the body of the function is just a return statement.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
     return accumulate(mul, 1, n, term)
 
 
-def compose1(func1, func2):
-    """Return a function f, such that f(x) = func1(func2(x))."""
-    def f(x):
-        return func1(func2(x))
-    return f
-
-
-def make_repeater(func, n):
-    """Return the function that computes the nth application of func.
+def make_repeater(f, n):
+    """Returns the function that computes the nth application of f.
 
     >>> add_three = make_repeater(increment, 3)
     >>> add_three(5)
     8
-    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    >>> make_repeater(triple, 5)(1) # 3 * (3 * (3 * (3 * (3 * 1))))
     243
     >>> make_repeater(square, 2)(5) # square(square(5))
     625
-    >>> make_repeater(square, 4)(5) # square(square(square(square(5))))
-    152587890625
-    >>> make_repeater(square, 0)(5) # Yes, it makes sense to apply the function zero times!
-    5
+    >>> make_repeater(square, 3)(5) # square(square(square(5)))
+    390625
     """
     "*** YOUR CODE HERE ***"
-    return accumulate(compose1, identity, n, lambda x: func)
-
-
-def zero(f):
-    return lambda x: x
-
-def successor(n):
-    return lambda f: lambda x: f(n(f)(x))
-
-def one(f):
-    """Church numeral 1: same as successor(zero)"""
-    "*** YOUR CODE HERE ***"
-    def fx(x):
-        return f(x)
-    return fx
-def two(f):
-    """Church numeral 2: same as successor(successor(zero))"""
-    "*** YOUR CODE HERE ***"
-    def ffx(x):
-        return f(f(x))
-    return ffx
-
-three = successor(two)
-
-def church_to_int(n):
-    """Convert the Church numeral n to a Python integer.
-
-    >>> church_to_int(zero)
-    0
-    >>> church_to_int(one)
-    1
-    >>> church_to_int(two)
-    2
-    >>> church_to_int(three)
-    3
-    """
-    "*** YOUR CODE HERE ***"
-    def add1(x):
-        return x+1
-    return n(add1)(0)
-
-def add_church(m, n):
-    """Return the Church numeral for m + n, for Church numerals m and n.
-
-    >>> church_to_int(add_church(two, three))
-    5
-    """
-    "*** YOUR CODE HERE ***"
-    int_n = church_to_int(n)
-    for i in range(int_n):
-        m = successor(m)
-    return m
-
-
-def mul_church(m, n):
-    """Return the Church numeral for m * n, for Church numerals m and n.
-
-    >>> four = successor(three)
-    >>> church_to_int(mul_church(two, three))
-    6
-    >>> church_to_int(mul_church(three, four))
-    12
-    """
-    "*** YOUR CODE HERE ***"
-    ans = zero
-    int_n = church_to_int(n)
-    for i in range(int_n):
-        ans = add_church(ans, m)
-    return ans
-
-def pow_church(m, n):
-    """Return the Church numeral m ** n, for Church numerals m and n.
-
-    >>> church_to_int(pow_church(two, three))
-    8
-    >>> church_to_int(pow_church(three, two))
-    9
-    """
-    "*** YOUR CODE HERE ***"
-    ans = one
-    int_n = church_to_int(n)
-    for i in range(int_n):
-        ans = mul_church(ans, m)
-    return ans
-    
-
+    def repeater(x):
+        result = x
+        for i in range(n):
+            result = f(result)
+        return result
+    return repeater
